5196b122f7c3faa72fe04a2b8272f7fb843e04aa
HARSHITHA RAJAMURI <rajamuriharshitha@HARSHITHAs-MacBook-Air.local>
2025-06-17 00:11:59 +0530

    done few changes

--- Design_principles_patterns/Exercise6/Main.java
+++ Design_principles_patterns/Exercise6/Main.java
@@ -1,6 +1,4 @@
 package Exercise6;
-
-
   public class Main {
   public static void main(String[] args) {
     Image i1=new Proxy("dog.png");
@@ -11,5 +9,4 @@ package Exercise6;
     i1.display();//caching
 
   }
-  
 }
