46fde138e28e5297b6d1320b9917f97b58dc1bba
HARSHITHA RAJAMURI <rajamuriharshitha@HARSHITHAs-MacBook-Air.local>
2025-07-20 22:02:22 +0530

    added week 5 hands on solution

---
+++ WEEk5/Microservices with API gateway/loan/src/main/java/com/cognizant/loan/contoller/LoanController.java
@@ -0,0 +1,42 @@
+package com.cognizant.loan.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("loans")
+
+public class LoanController {
+	@GetMapping("/{number}")
+	public Loan getLoanDetails(@PathVariable String number)
+	{
+		return new Loan(number,"car",400000,3258,18);
+	}
+	static class Loan{
+		private String number;
+        private String type;
+        private double loan;
+        private int emi;
+        private int tenure;
+        public Loan(String number, String type, double loan, int emi, int tenure) {
+
+        this.number = number;
+        this.type = type;
+        this.loan = loan;
+        this.emi = emi;
+        this.tenure = tenure;
+		
+	}
+	
+        public String getNumber() { return number; }
+        public String getType() { return type; }
+        public double getLoan() { return loan; }
+        public int getEmi() { return emi; }
+        public int getTenure() { return tenure; }
+    }
+	
+	
+
+}
