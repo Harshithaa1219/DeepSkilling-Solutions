bb5fe9853c1ce47c0a4436ad99159c219c83065e
HARSHITHA RAJAMURI <rajamuriharshitha@HARSHITHAs-MacBook-Air.local>
2025-07-26 22:24:08 +0530

    added week 6 2nd asssignment

---
+++ WEEK6/2.ReactJs-HOL solution/code/studentapp/src/Components/Home.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+class Home extends Component
+{
+  render()
+  {
+    return <h2>welcome to the home page of student management portal</h2>
+  }
+}
+export default Home;
